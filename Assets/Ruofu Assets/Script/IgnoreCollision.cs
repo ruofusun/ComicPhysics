692bba948439b0732c849882f837f7d849abd714
ruofusun <42979525+ruofusun@users.noreply.github.com>
2022-11-09 00:32:43 -0500

    camera transition for level 1 ending

--- Assets/Ruofu Assets/Script/IgnoreCollision.cs
+++ Assets/Ruofu Assets/Script/IgnoreCollision.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class IgnoreCollision : MonoBehaviour
 {
+
+    public List<Collider2D> colsToIgnore;
+
+    private Collider2D mycol;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mycol = GetComponent<Collider2D>();
+        if (colsToIgnore.Count > 0)
+        {
+            foreach (var col in colsToIgnore)
+            {
+                Physics2D.IgnoreCollision(mycol, col);
+            }
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/Ruofu Assets/Script/TriggerTimeline.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TriggerTimeline : MonoBehaviour
+{
+
+    public LayerMask layer;
+
+    public PlayableDirector playableDirector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        int otherLayer = 1 << other.gameObject.layer;
+        Debug.Log(otherLayer+" "+ layer);
+        if (1<<other.gameObject.layer == layer)
+        {
+            playableDirector.Play();
+        }
+    }
+}
