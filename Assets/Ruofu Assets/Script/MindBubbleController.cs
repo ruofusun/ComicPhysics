1d8931401c5664394c519326a921c5388fde0a8a
ruofusun <42979525+ruofusun@users.noreply.github.com>
2022-11-10 16:18:39 -0500

    dotween and mindbubble joints

--- Assets/Ruofu Assets/Script/MindBubbleController.cs
+++ Assets/Ruofu Assets/Script/MindBubbleController.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using DG.Tweening;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class MindBubbleController : MonoBehaviour
@@ -9,6 +12,8 @@ public class MindBubbleController : MonoBehaviour
     public Rigidbody2D centerRB;
 
     public Transform destination;
+
+    public float duration;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +23,39 @@ public class MindBubbleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      
         
     }
 
-    public void MoveToDesiredPosition()
+    [Button]
+    public void StartMoving()
     {
-       
+       // centerRB.DOMove(destination.position, duration).OnComplete(OnTweenComplete);
+       /* foreach (var rb2d in rb2ds)
+        {
+            rb2d.gravityScale = -0.05f;
+        }
+*/
+       centerRB.gravityScale = -0.6f;
+
+    }
+    
+
+
+    public void OnTweenComplete()
+    {
+        foreach (var rb2d in rb2ds)
+            {
+                rb2d.gravityScale = 0f;
+            }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            centerRB.gravityScale = -1.2f;
+        }
     }
 }
